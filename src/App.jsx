851e8d6276a165f4965f765bf86990cc8ec05df5
Rebekah Callari-Kaczmarczyk <rkaczmarczyk@phillies.com>
2024-07-10 20:11:24 -0400

    :memo: add inputs and lift button state

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,64 @@
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './Button'
 
+// This function takes a string and splits it by a space
+// This is an example of a function that returns an array
+const getName = ((name) => {
+  const nameSplit = name.split(" ")
+  return [nameSplit[0], nameSplit[1]]
+})
+
 function App() {
+  // Going to start this value at 0 (initial state)
+  const [books, setBooks] = useState(0)
+  // Going to start this value at 1 (initial state)
+  const [audiobooks, setAudiobooks] = useState(1)
+  // This state is in order to controll the input value
+  const [inputValue, setInputValue] = useState();
+  // Here we are destructuring the array returned by getName
+  const [firstName, lastName] = getName("Rebekah Callari");
+
+  // This is a handler function to add one to books
+  const handleClickAddBooks = () => {
+    setBooks(books + 1)
+  };
+
+  // This is a handler function to subtract one from books
+  const handleClickSubtractBooks = () => {
+    setBooks(books - 1)
+  };
+
+  // This is a handler function to add one to audiobooks
+  const handleClickAddAudiobooks = () => {
+    setAudiobooks(audiobooks + 1)
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h3>{`Owner: ${lastName ?? "-"}, ${firstName ?? "-"}`}</h3>
+      <h3>Total of Count 1: </h3>
+      <h3>{books}</h3>
+      {/* These two buttons both update books */}
       <div className="card">
-        <Button title={"Button 1"}/>
-        <Button title={"Button 2"}/>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Button title={"Add 1 book"} handleClick={handleClickAddBooks}/>
+        <Button title={"Subtract 1 book"} handleClick={handleClickSubtractBooks}/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Total of Count 2: </h3>
+      <h3>{audiobooks}</h3>
+      {/* This button updates audiobooks */}
+      <Button title={"Add 1 audiobook"} handleClick={handleClickAddAudiobooks}/>
+      <p>Sample Input</p>
+      <p>Controlled Input</p>
+      {/* This is a controlled input because the value is defined by inputValue (a variable) */}
+      <input value={inputValue} onChange={(event) => setInputValue(event.target.value)}></input>
+      {/* This is JUST an example (don't do this) -> this is not a controlled input 
+      but on change, it calls the setState function for the controlled input above.
+      If you type in this input, you'll see the other input value change */}
+      <p>Not a controlled input but updates the other input when it changes</p>
+      <input onChange={(event) => setInputValue(event.target.value)}></input>
     </>
   )
 }
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
+
 function Button(props) {
-    const [count, setCount] = useState(0)
+
     return (
         <>
-        <button onClick={() => setCount((count) => count + 1)}>
-          {props.title} count is {count}
+        <button onClick={props.handleClick}>
+          {props.title}
         </button>
-        {count > 2 && <h3>The count is greater than 2!</h3>}
         </>
     )
 }
